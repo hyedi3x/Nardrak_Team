b4c83a7a3f2fc4bea8ff45ad1865e65109c7f445
hyeonw12 <rlatkdgus100@naver.com>
2025-02-01 17:38:05 +0900

    [feat] AdminDTO.java
    - 관리자 정보 관련 변수 선언

---
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dto/AdminDTO.java
@@ -0,0 +1,155 @@
+package com.spring.nardrak_mvc.dto;
+
+import java.sql.Date;
+
+public class AdminDTO {
+	
+	private String ad_id;			// 아이디
+	private String ad_pwd; 			// 비밀번호
+	private String ad_name; 		// 이름
+	private String ad_birth; 		// 생년월일
+	private String ad_phone; 		// 통신사 & 전화번호
+	private String ad_email; 		// 이메일
+	private String ad_tel; 			// 사내 전화번호
+	private String ad_cpnum; 		// 사번
+	private String ad_dep; 			// 부서코드
+	private Date ad_regDate; 		// 가입일
+	private String ad_terms; 		// 약관동의 항목
+	private String ad_permission; 	// 권한상태
+	private String l_session; 		// 계정권한 (admin)
+	
+	// 디폴트 생성자
+	public AdminDTO() {}
+	
+	// 매개 변수
+	public AdminDTO(String ad_id, String ad_pwd, String ad_name, String ad_birth, String ad_phone, String ad_email,
+			String ad_tel, String ad_cpnum, String ad_dep, Date ad_regDate, String ad_terms, String ad_permission,
+			String l_session) {
+		super();
+		this.ad_id = ad_id;
+		this.ad_pwd = ad_pwd;
+		this.ad_name = ad_name;
+		this.ad_birth = ad_birth;
+		this.ad_phone = ad_phone;
+		this.ad_email = ad_email;
+		this.ad_tel = ad_tel;
+		this.ad_cpnum = ad_cpnum;
+		this.ad_dep = ad_dep;
+		this.ad_regDate = ad_regDate;
+		this.ad_terms = ad_terms;
+		this.ad_permission = ad_permission;
+		this.l_session = l_session;
+	}
+
+	public String getAd_id() {
+		return ad_id;
+	}
+
+	public void setAd_id(String ad_id) {
+		this.ad_id = ad_id;
+	}
+
+	public String getAd_pwd() {
+		return ad_pwd;
+	}
+
+	public void setAd_pwd(String ad_pwd) {
+		this.ad_pwd = ad_pwd;
+	}
+
+	public String getAd_name() {
+		return ad_name;
+	}
+
+	public void setAd_name(String ad_name) {
+		this.ad_name = ad_name;
+	}
+
+	public String getAd_birth() {
+		return ad_birth;
+	}
+
+	public void setAd_birth(String ad_birth) {
+		this.ad_birth = ad_birth;
+	}
+
+	public String getAd_phone() {
+		return ad_phone;
+	}
+
+	public void setAd_phone(String ad_phone) {
+		this.ad_phone = ad_phone;
+	}
+
+	public String getAd_email() {
+		return ad_email;
+	}
+
+	public void setAd_email(String ad_email) {
+		this.ad_email = ad_email;
+	}
+
+	public String getAd_tel() {
+		return ad_tel;
+	}
+
+	public void setAd_tel(String ad_tel) {
+		this.ad_tel = ad_tel;
+	}
+
+	public String getAd_cpnum() {
+		return ad_cpnum;
+	}
+
+	public void setAd_cpnum(String ad_cpnum) {
+		this.ad_cpnum = ad_cpnum;
+	}
+
+	public String getAd_dep() {
+		return ad_dep;
+	}
+
+	public void setAd_dep(String ad_dep) {
+		this.ad_dep = ad_dep;
+	}
+
+	public Date getAd_regDate() {
+		return ad_regDate;
+	}
+
+	public void setAd_regDate(Date ad_regDate) {
+		this.ad_regDate = ad_regDate;
+	}
+
+	public String getAd_terms() {
+		return ad_terms;
+	}
+
+	public void setAd_terms(String ad_terms) {
+		this.ad_terms = ad_terms;
+	}
+
+	public String getAd_permission() {
+		return ad_permission;
+	}
+
+	public void setAd_permission(String ad_permission) {
+		this.ad_permission = ad_permission;
+	}
+
+	public String getL_session() {
+		return l_session;
+	}
+
+	public void setL_session(String l_session) {
+		this.l_session = l_session;
+	}
+	
+	@Override
+	public String toString() {
+		return "AdminDTO [ad_id=" + ad_id + ", ad_pwd=" + ad_pwd + ", ad_name=" + ad_name + ", ad_birth=" + ad_birth
+				+ ", ad_phone=" + ad_phone + ", ad_email=" + ad_email + ", ad_tel=" + ad_tel + ", ad_cpnum=" + ad_cpnum
+				+ ", ad_dep=" + ad_dep + ", ad_regDate=" + ad_regDate + ", ad_terms=" + ad_terms + ", ad_permission="
+				+ ad_permission + ", l_session=" + l_session + "]";
+	}
+}
