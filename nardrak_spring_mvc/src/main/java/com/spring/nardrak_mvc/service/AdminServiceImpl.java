fe9cf3945dade7f6a4b32a57f4c6e69e27b37755
hyeonw12 <rlatkdgus100@naver.com>
2025-02-02 20:24:34 +0900

    [fix] AdminService.impl.java -  dto를 전역변수로 선언 후 주석 추가 - 불필요한 System.out.println 제거 - ad_phone 변수에 대입 되는 값에 대한 설명 주석 추가 - 약관 동의 항목 값들이 ad_terms로 변환되는 과정 설명 주석 추가 - model.addAtrribute 구문 통일

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/AdminServiceImpl.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/AdminServiceImpl.java
@@ -23,6 +23,9 @@ public class AdminServiceImpl implements AdminService{
 	@Autowired
 	private AdminDAO dao;
 	
+	// 관리자 정보 전송 객체 통일성을 위해 전역변수로 선언
+	AdminDTO dto = new AdminDTO();
+	
 	// 아이디 중복확인
 	@Override
 	public void adminIdConfirm(HttpServletRequest request, HttpServletResponse response, Model model)
@@ -34,7 +37,6 @@ public class AdminServiceImpl implements AdminService{
 
 		model.addAttribute("selectCnt", dao.adminIdConfirm(ad_id));
 		model.addAttribute("ad_id", ad_id);
-		System.out.println(request.getAttribute("selectCnt"));
 	}
 
 	//관리자 회원가입
@@ -44,8 +46,6 @@ public class AdminServiceImpl implements AdminService{
 		
 		System.out.println("Service AdminSignUpAction");
 		
-		AdminDTO dto = new AdminDTO();
-		
 		dto.setAd_id(request.getParameter("ad_id")); 			// 아이디
 		dto.setAd_pwd(request.getParameter("ad_pwd")); 			// 비밀번호
 		dto.setAd_name(request.getParameter("ad_name")); 		// 이름
@@ -54,7 +54,8 @@ public class AdminServiceImpl implements AdminService{
 		String ad_phone1 = request.getParameter("ad_phone1"); 	// 통신사 
 		String ad_phone2 = request.getParameter("ad_phone2"); 	// 번호 (-없이 입력)
 		
-		// ex.010-1111-2222으로 DB에 값 저장
+		// 통신사(ex. S, K, L) 000-0000-0000 형식으로 DB에 값 저장
+		// ad_phone = 통신사데이터 + 공백 + 휴대폰번호 앞에서부터 3자리 + '-' + 4자리 + '-' 4자리
 		String ad_phone = ad_phone1+" "
 										 +ad_phone2.substring(0,3)+"-"
 										 +ad_phone2.substring(3,7)+"-"
@@ -67,27 +68,31 @@ public class AdminServiceImpl implements AdminService{
 		dto.setAd_email(ad_email);
 		
 		dto.setAd_tel(request.getParameter("ad_tel"));			// 사내 전화번호 (필수X)
-		dto.setAd_cpnum(request.getParameter("ad_cpnum"));		// 사번
+		dto.setAd_empnum(request.getParameter("ad_empnum"));		// 사번
 		dto.setAd_dep(request.getParameter("ad_dep"));			// 부서코드
 		
-		List<String> chkArr = new ArrayList<String>();			// 약관동의 항목
-		chkArr.add(request.getParameter("chkList1")); 			
-		chkArr.add(request.getParameter("chkList2")); 
-		chkArr.add(request.getParameter("chkList3")); 
-		chkArr.add(request.getParameter("chkList4")); 
+		// 약관동의 항목을 담을 ArrayList
+		List<String> chkArr = new ArrayList<String>();			
+
+		// 각 약관 순서대로 체크된 요소만 값을 1~4로 전달함.
+		chkArr.add(request.getParameter("chkList1")); // value=1	
+		chkArr.add(request.getParameter("chkList2")); // value=2	
+		chkArr.add(request.getParameter("chkList3")); // value=3	
+		chkArr.add(request.getParameter("chkList4")); // value=4	
 		
+		// 향상된 for문으로  List에 담은 약관 값이 존재하는 경우(체크한 경우)에만 값+공백을 실행
 		String ad_terms = "";
 		for(String index:chkArr) {
 			if(index != null) {
 				ad_terms += index+" ";
 			}
 		}
+		// 마지막에 추가된 공백을 지워준다.
 		ad_terms = ad_terms.substring(0,ad_terms.length()-1);
 	
 		dto.setAd_terms(ad_terms);
 		
-		int insertCnt = dao.adminSignUpAction(dto);
-		model.addAttribute("insertCnt", insertCnt);
+		model.addAttribute("insertCnt",  dao.adminSignUpAction(dto));
 	}
 
 }
