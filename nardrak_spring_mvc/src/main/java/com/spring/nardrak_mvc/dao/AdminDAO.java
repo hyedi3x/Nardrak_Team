a11e663926a9c83b3c1e5b5ff7bedc51ec50ec05
hyeonw12 <rlatkdgus100@naver.com>
2025-02-02 20:18:54 +0900

    [fix] AdminDAO.java
    - 매개변수 dto에 대한 설명 주석 추가

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/AdminDAO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/AdminDAO.java
@@ -10,6 +10,7 @@ public interface AdminDAO {
 		public int adminIdConfirm(String ad_id);
 		
 		// 관리자 회원가입
+		// dto : 뷰 객체에서 전송한 input 값을 멤버 변수로 담은 클래스, DB로 전송하기 위함.
 		public int adminSignUpAction(AdminDTO dto);
 
 }
