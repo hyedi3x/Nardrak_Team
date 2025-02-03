40dfed8eed28c28c641db0e97f43c5ee14bdebba
cruelSummer12 <rlatkdgus100@naver.com>
2025-02-03 21:35:08 +0900

    [fix] AdminDTO.java - 가입일 자료형 Date를 Timestamp로 변경

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dto/AdminDTO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dto/AdminDTO.java
@@ -1,6 +1,6 @@
 package com.spring.nardrak_mvc.dto;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 
 public class AdminDTO {
 	
@@ -11,10 +11,11 @@ public class AdminDTO {
 	private String ad_birth; 		// 생년월일
 	private String ad_phone; 		// 통신사 & 전화번호
 	private String ad_email; 		// 이메일
+	private String ad_zip; 		    // 우편번호, 기본주소, 상세주소 
 	private String ad_tel; 			// 사내 전화번호
 	private String ad_empnum; 		// 사번
 	private String ad_dep; 			// 부서코드
-	private Date ad_regDate; 		// 가입일
+	private Timestamp ad_regDate; 		// 가입일
 	private String ad_terms; 		// 약관동의 항목
 	private String ad_permission; 	// 권한상태
 	private String l_session; 		// 계정권한 (admin)
@@ -22,10 +23,10 @@ public class AdminDTO {
 	// 디폴트 생성자
 	public AdminDTO() {}
 	
-	// 매개 변수
+	// 매개변수 생성자
 	public AdminDTO(String ad_id, String ad_pwd, String ad_name, String ad_birth, String ad_phone, String ad_email,
-			String ad_tel, String ad_empnum, String ad_dep, Date ad_regDate, String ad_terms, String ad_permission,
-			String l_session) {
+			String ad_zip, String ad_tel, String ad_empnum, String ad_dep, Timestamp ad_regDate, String ad_terms,
+			String ad_permission, String l_session) {
 		super();
 		this.ad_id = ad_id;
 		this.ad_pwd = ad_pwd;
@@ -33,6 +34,7 @@ public class AdminDTO {
 		this.ad_birth = ad_birth;
 		this.ad_phone = ad_phone;
 		this.ad_email = ad_email;
+		this.ad_zip = ad_zip;
 		this.ad_tel = ad_tel;
 		this.ad_empnum = ad_empnum;
 		this.ad_dep = ad_dep;
@@ -41,7 +43,7 @@ public class AdminDTO {
 		this.ad_permission = ad_permission;
 		this.l_session = l_session;
 	}
-	
+
 	// getter, setter
 	public String getAd_id() {
 		return ad_id;
@@ -91,6 +93,14 @@ public class AdminDTO {
 		this.ad_email = ad_email;
 	}
 
+	public String getAd_zip() {
+		return ad_zip;
+	}
+
+	public void setAd_zip(String ad_zip) {
+		this.ad_zip = ad_zip;
+	}
+
 	public String getAd_tel() {
 		return ad_tel;
 	}
@@ -115,11 +125,11 @@ public class AdminDTO {
 		this.ad_dep = ad_dep;
 	}
 
-	public Date getAd_regDate() {
+	public Timestamp getAd_regDate() {
 		return ad_regDate;
 	}
 
-	public void setAd_regDate(Date ad_regDate) {
+	public void setAd_regDate(Timestamp ad_regDate) {
 		this.ad_regDate = ad_regDate;
 	}
 
@@ -151,8 +161,8 @@ public class AdminDTO {
 	@Override
 	public String toString() {
 		return "AdminDTO [ad_id=" + ad_id + ", ad_pwd=" + ad_pwd + ", ad_name=" + ad_name + ", ad_birth=" + ad_birth
-				+ ", ad_phone=" + ad_phone + ", ad_email=" + ad_email + ", ad_tel=" + ad_tel + ", ad_empnum=" + ad_empnum
-				+ ", ad_dep=" + ad_dep + ", ad_regDate=" + ad_regDate + ", ad_terms=" + ad_terms + ", ad_permission="
-				+ ad_permission + ", l_session=" + l_session + "]";
+				+ ", ad_phone=" + ad_phone + ", ad_email=" + ad_email + ", ad_zip=" + ad_zip + ", ad_tel=" + ad_tel
+				+ ", ad_empnum=" + ad_empnum + ", ad_dep=" + ad_dep + ", ad_regDate=" + ad_regDate + ", ad_terms="
+				+ ad_terms + ", ad_permission=" + ad_permission + ", l_session=" + l_session + "]";
 	}
 }
