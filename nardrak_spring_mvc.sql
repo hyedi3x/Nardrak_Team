fa511f34934df8e132314422fbf1514fc4d98092
hyeonw12 <rlatkdgus100@naver.com>
2025-02-03 23:39:52 +0900

    [fix]  nardrak_spring_mvc.sql - 주소 형식 변경 - id VARCHAR2 사이즈 10으로 변경 -  사번 컬럼명 ad_empnum으로 변경 - 가입입 TIMESTAMP로 변경 - login_session으로 FK명 변경 후  DEFAULT 값 제거 - INSERT에 login_session 추가

--- nardrak_spring_mvc.sql
+++ nardrak_spring_mvc.sql
@@ -1,59 +1,68 @@
--- 세션 테이블 생성
--- 관리자, 고객
-DROP TABLE session_tbl;
--- 세션 테이블 pk : Admin,Customer 구분, 그밖에 세션과 관련된 정보 넣을 테이블 
-CREATE TABLE session_tbl(
-    l_session   VARCHAR2(30) PRIMARY KEY,           -- customer, admin 구분 용
-    session_num    NUMBER(2)    UNIQUE     NOT NULL    
-);
+-- [ 1. 세션 테이블 컬럼 넘버 추가 ] --
 
-SELECT * FROM session_tbl;
+-- NOT NULL 컬럼을 추가 하기 위한 나머지 컬럼 초기화
+DELETE login_tb;
 
-INSERT INTO session_tbl (session_num, l_session)
-    VALUES (NVL((SELECT MAX(session_num)+1 FROM session_tbl),1), '관리자');
-    
-UPDATE session_tbl
-SET l_session = '관리자'
-WHERE session_num = 1;
+-- 컬럼 별 번호 추가
+ALTER TABLE login_tb
+ADD session_num NUMBER(2)  UNIQUE  NOT NULL; 
 
-DELETE session_tbl
-WHERE session_num = 1;
+-- 관리자, 고객 컬럼 추가
+INSERT INTO login_tb (session_num, login_session)
+    VALUES (NVL((SELECT MAX(session_num)+1 FROM login_tb),1), 'Admin');
 
--- 관리자 계정 생성 TBL
+INSERT INTO login_tb (session_num, login_session)
+    VALUES (NVL((SELECT MAX(session_num)+1 FROM login_tb),1), 'Customer');
 
--- 아이디  : 5~20자 영문 소문자, 숫자, 특수기호 _
+-- 결과 조회
+SELECT * FROM login_tb;
+
+-- [ 2. 관리자 등록 ] --
+
+-- 테이블 생성
+-- 아이디  : 영어 소문자, 숫자로만 4~10자
 -- 비밀번호 : 8~16자 영문 대/소문자 숫자 특수문자 
--- 생년월일: 8자리 숫자
--- 성별 필수 (남, 여, 선택안함)
--- 나이 : 만 14세 미만 불가
--- 핸드폰 : 관리자 계정은 필수
--- 관리자 타입 : admin_dep : 부서를 넣는 컬럼 
-DROP TABLE admin_tbl;
-CREATE TABLE admin_tbl(
-    ad_id      VARCHAR2(20)    PRIMARY KEY,     -- 아이디
-    ad_pwd     VARCHAR2(16)    NOT NULL,       -- 비밀번호
-    ad_name    VARCHAR2(30)    NOT NULL,       -- 이름
-    ad_birth   VARCHAR2(8)     NOT NULL,       -- 생년월일-성별
-    ad_phone   VARCHAR2(20)    UNIQUE  NOT NULL,   -- 통신사 & 전화번호
-    ad_email   VARCHAR2(30)    UNIQUE  NOT NULL,   -- 이메일
-    ad_tel     VARCHAR2(13)    UNIQUE,             -- 사내 전화번호
-    ad_cpnum   VARCHAR2(20)    UNIQUE  NOT NULL,-- 사번
-    ad_dep     VARCHAR2(30)    NOT NULL,       -- 부서코드
-    ad_regDate DATE    DEFAULT sysdate,        -- 가입일
-    ad_terms    VARCHAR2(8)    NOT NULL,       -- 약관동의 항목
-    ad_permission  CHAR(1) DEFAULT 'N'     NOT NULL, -- 권한상태
-    l_session VARCHAR2(20) DEFAULT '관리자' NOT NULL,     -- 권한 세션
-    CONSTRAINT admin_tbl_fk FOREIGN KEY(l_session) REFERENCES session_tbl(l_session)
+-- 이름
+-- 생년월일 : 6자리 & 만 14세 미만 등록 불가
+-- 핸드폰 : 통신사 000-0000-0000 형식
+-- 이메일 : 000@000 형식
+-- 주소 : 우편번호, 기본주소, 상세주소 형식
+-- 사내전화번호 : 9~11자
+-- 사번
+-- 부서코드
+-- 가입일 : DEFAULT sysdate
+-- 약관동의: 1 2 3 4 형식
+-- 권한 상태 : DEFAULT 'Y'  -> DEFAULT 'N' 
+-- 권한 세션 : DEFAULT 'Admin', FOREIGN KEY, REPERNECES login_tb(login_session), ON DELETE CASCADE
+DROP TABLE admin_tb;
+CREATE TABLE admin_tb(
+    ad_id          VARCHAR2(10)    PRIMARY KEY,        -- 아이디
+    ad_pwd         VARCHAR2(16)    NOT NULL,           -- 비밀번호
+    ad_name        VARCHAR2(30)    NOT NULL,           -- 이름
+    ad_birth       VARCHAR2(6)     NOT NULL,           -- 생년월일-성별
+    ad_phone       VARCHAR2(20)    UNIQUE  NOT NULL,   -- 통신사 & 전화번호
+    ad_email       VARCHAR2(30)    UNIQUE  NOT NULL,   -- 이메일
+    ad_zip         VARCHAR2(250)   NOT NULL,           -- 우편번호, 기본주소, 상세주소
+    ad_tel         VARCHAR2(13),                       -- 사내 전화번호
+    ad_empnum      VARCHAR2(20)   UNIQUE  NOT NULL,    -- 사번
+    ad_dep         VARCHAR2(30)    NOT NULL,           -- 부서코드
+    ad_regDate     TIMESTAMP    DEFAULT sysdate,       -- 가입일
+    ad_terms       VARCHAR2(8)    NOT NULL,            -- 약관동의 항목
+    delete_status  CHAR(1) DEFAULT 'N'     NOT NULL,   -- 권한상태 -> default에다가 인풋값이 아닌 컬럼이기에 기본 not null 상태, not null 조건 안주셔도 됩니다 
+    login_session  VARCHAR2(20),                       -- 권한 세션
+    CONSTRAINT admin_tb_fk FOREIGN KEY(login_session) REFERENCES login_tb(login_session)
     ON DELETE CASCADE
 );
 
-SELECT * FROM admin_tbl
-WHERE ad_id = 1
-AND ad_pwd = 1
-AND ad_permission = 'N';
+-- 관리자 등록 시 아이디 중복 확인
+SELECT COUNT(*) FROM admin_tb
+WHERE ad_id = '아이디';
 
--- 회원가입
-INSERT INTO admin_tbl(ad_id, ad_pwd, ad_name, ad_birth, ad_phone, ad_email, ad_tel, ad_cpnum, ad_dep, ad_terms)
-    VALUES('아이디2', '1234567891', '홍길동', '000101', '010-1222-5678','seoul2@visit.com', '02-1122-2222', 'A1111112', '자연관광부서', '1 2 3 4');
+-- 관리자 등록 실행
+INSERT INTO admin_tb(ad_id, ad_pwd, ad_name, ad_birth, ad_phone, ad_email, ad_zip, ad_tel, ad_empnum, ad_dep, ad_terms, login_session)
+    VALUES('hyeri', '1234567891', '홍길동', '000101', '010-1222-2222','seoul3@visit.com', '0000, 기본주소, 상세주소', '02-1234-1234', 'A1111113', '자연관광부서', '1 2 3 4', 'Admin');
+
+-- 등록 확인
+SELECT * FROM admin_tb;
 
 COMMIT;
