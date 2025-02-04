9533c3e875fc48fac0a77a38e501d4d03639d846
hyedi3x <hyedi3x@gmail.com>
2025-02-04 13:46:51 +0900

    [feat] CustomerService.java(로그인 성공 처리 메서드 추가)
    - loignResult 메서드에서 파라미터로 input 값들을 저장하고, 저장한 input 값을 dao로 전달해 아이디와 비밀번호를 확인한다.
    - 아이디, 비밀번호, 세션에 대한 값들을 변수로 저장해, request.getSession().setAttribute로 저장한다.

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/CustomerService.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/CustomerService.java
@@ -1,5 +1,17 @@
 package com.spring.nardrak_mvc.service;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+
 public interface CustomerService {
+
+	// ======================= [로그인 성공 처리 페이지] =======================
+	public void loginResult(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException;
 
 }
