6d8ea42c7738fdc68f2cc4d3ab51fc8ec05ce10e
hyedi3x <hyedi3x@gmail.com>
2025-02-04 13:46:51 +0900

    [feat] CustomerService.java(로그인 성공 처리 메서드 추가)
    - loignResult 메서드에서 파라미터로 input 값들을 저장하고, 저장한 input 값을 dao로 전달해 아이디와 비밀번호를 확인한다.
    - 아이디, 비밀번호, 세션에 대한 값들을 변수로 저장해, request.getSession().setAttribute로 저장한다.

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/CustomerServiceImpl.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/CustomerServiceImpl.java
@@ -1,15 +1,57 @@
 package com.spring.nardrak_mvc.service;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import com.spring.nardrak_mvc.dao.CustomerDAO;
 
 @Service
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService{
 
 	@Autowired
 	private CustomerDAO dao;
 	
+	@Override
+	// ======================= [로그인 성공 처리 페이지] =======================
+	public void loginResult(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("CustomerSerivceImpl - loginResult()");
+
+		// -------------[3단계] 화면에서 입력받은 값을 가져와서 파라미터로 담기 -------------
+		String strId = request.getParameter("user_id");
+		String strPwd = request.getParameter("user_pwd");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("strId", strId);   // key, value
+		map.put("strPwd", strPwd);
+				
+		// -------------[4단계] - 전역변수로 선언-------------
+		// -------------[5단계] 로그인 성공여부 처리-------------
+		// 아이디와 비밀번호가 일치하는지 userIdPwdChk 메서드 실행후 결과값을 selectCnt 변수에 담는다.
+		int loginCnt = dao.userIdPwdChk(map);
+		int sessionCnt = dao.sessionResult(strId);
+				
+		// -------------[6단계] - selectCnt 값에 따라 세션 설정 및 에러 처리 ------------
+		// sessionCnt 값에 따라 세션 설정 및 에러 처리
+		// request.getSession() : 컨트롤러 호출 시 세션이 존재하면 존재하는 세션을 전달, 없으면 새로 생성 또는 null 반환
+		if (loginCnt == 1) {		
+			if (sessionCnt == 0) { // 일반 회원	
+				request.getSession().setAttribute("sessionID", strId);
+				request.getSession().setAttribute("login_session", "Customer"); // 사용자 유형 저장
+			} else if (sessionCnt == 1) { // 관리자
+				request.getSession().setAttribute("sessionID", strId);
+				request.getSession().setAttribute("login_session", "Admin"); // 사용자 유형 저장
+			}
+		}
+	}
 	
 }
