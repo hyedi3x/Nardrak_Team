2b13b411127a5649076aecdde78c882bacf1a8e5
kilminjong <qkqk0110@naver.com>
2025-02-04 20:31:23 +0900

    [feat] CustomerDAO.java(ID 중복확인)
    - DAO 추상메서드 생성 및 구현
    - serviceImpl에서 받아온 cs_id를 useridCheck메서드에 담아서 mapper로 전달.

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/CustomerDAO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/CustomerDAO.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 public interface CustomerDAO {
 	
+	// ======================= [ID 중복확인 처리] =======================
+	public int useridCheck(String cs_id);
+	
 	// ======================= [회원정보 인증(아이디, 비번)] =======================
 	public int userIdPwdChk(Map<String, Object> map);
 	
