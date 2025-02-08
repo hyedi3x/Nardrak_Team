7b1c65c8c378dc99da19ec5d1d95b8b582c4d928
hyeonw12 <rlatkdgus100@naver.com>
2025-02-08 19:44:31 +0900

    [feat] join.js
    - submit  함수에서 unique 값이 존재하는지 판단한다. ( ajax를 통해 input hidden을 만들어 생성된 input 요소의 값을 조건으로 확인)
    - 경고창 오류들 수정
    - 성별 기능 추가 , 생년월일 입력이 6자리가 되면 3항식으로 성별을 1,2 또는 3,4 로 받을 지 결정, 결정된 조건에 따라 00년생 전후로 성별 입력 제한
    - 유니크 컬럼을 ajax로 휴대폰, 이메일, 사번의 유니크 판별을 비동기적으로  요청을 처리 하여 actionResult에서 조회된 값이 없을 경우에만 input요소를 생성해 input 요소의  value가 일치하지 않는 경우 서브밋 함수에서 return false를 실행

--- nardrak_spring_mvc/src/main/webapp/resources/js/admin/join/join.js
+++ nardrak_spring_mvc/src/main/webapp/resources/js/admin/join/join.js
@@ -38,7 +38,9 @@ function idConfirm3(){
 4. 비밀번호 체크
 5. 모달창 체크
 */
-
+// 핸드폰, 성별 유효성 체크용
+let phone_input = 0;
+let jender_input = 0;
 function submitChk(){
    let pw = $('#ad_pwd').val();
    let pwdChk = $('#pwdChk').val();
@@ -63,11 +65,13 @@ function submitChk(){
    // 필수 약관
    if($('#modalChk1').val() == 0 || $('#modalChk2').val() == 0){
       alert("필수 약관을 읽고 동의해주세요");
+      $('#modalChk1').focus();
       return false;
    }
    
-   if($('#ad_phone2').val().length != 11 ){
+   if(phone_input != 1 ){
       alert("전화번호 11자리를 정확히 입력해주세요.");
+      $('#ad_phone').focus();
       return false;
    }
    
@@ -94,7 +98,34 @@ function submitChk(){
       $('#ad_birth').focus();
       return false;
    }
-  
+   
+   if(!$('#ad_zip1').val() || !$('#ad_zip2').val()){
+   	alert('주소를 입력해주세요.');
+    $('#ad_ZIPBTN').focus();
+   	return false;
+  	}
+  	
+  	if(!(jender_input == 1 || jender_input == 2)){
+   	alert(jender_input+'주민번호 뒷자리를 확인해 주세요.');
+    $('#ad_jender').focus();
+   	return false;
+  	}
+  	// unique 폰, 이메일, 사번 ajax로 받아오기
+  	if($('#uniquePhone').val()!=1){
+   	alert('이미 등록된 핸드폰 번호입니다.');
+    $('#ad_phone2').focus();
+   	return false;
+  	}
+  	if($('#uniqueEmail').val()!=1){
+   	alert('이미 등록된 이메일입니다.');
+    $('#ad_email1').focus();
+   	return false;
+  	}
+  	if($('#uniqueEmpnum').val()!=1){
+   	alert('이미 등록된 사번입니다.');
+    $('#ad_empnum').focus();
+   	return false;
+  	}
 }
 
 // 비밀 번호 표시 - 아이콘 클릭시 비밀번호 이미지와 타입 변경
@@ -118,9 +149,6 @@ $(function(){
 	});
 });
 
-// 생년월일 체크용 입력확인과 maxlength에서 초기화
-let inputChk = 0;
-
 // 정규식 체크
 // 아이디, 이메일1, 이메일2 정규식
 const reg_id = /^(?=.*[a-z])[a-z\d]{4,10}$/;
@@ -129,227 +157,255 @@ const reg_email2 = /^[a-z]+\.[a-z]+$/;
 const reg_pwd = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[a-zA-Z0-9@$!%*#?&]{8,16}$/;  // 0개이상 문자뒤에 영소, 0~9, 특수기호 요구
 // 생년월일, 휴대폰 번호 입력 확인
 // 입력이 변할 때 마다 함수가 실행되어(실시간 함수 실행) 6자리가 입력되지 않으면 스타일을 변경하고 #birthChk 문구를 띄운다
-$(function(){
 
-	// 핸드폰
-	$('#ad_phone2').on('input', function(){
-		let ad_phone = $('#ad_phone2').val();
-		if(ad_phone.length == 0 || ad_phone.length == 11){
-			// 핸드폰 앞자리 010으로 통합됨
-			if(ad_phone.substring(0,3) == '010'){
-				$('#phoneChk').css('display', 'none');
-				$('#ad_phone2').css('border', '1px solid #d9d9d9');
-				$('#ad_phone2').css('outline', 'none');
-				$('#ad_phone2:hover').css('border-color', '#729ea1');
-			}
+	
+// input type이 number일 때, maxlength 속성이 존재하는 요소 선택
+$('input[type=number][maxlength]').on('input', function() { 
+    var num = $(this);                             // this = 이벤트 발생시킨 요소
+    var maxlength = num.attr('maxlength');         // maxlength 속성 값을 불러옴
+    var numval = num.val();
+    if (numval.length > maxlength) {
+        num.val(numval.substr(0, maxlength)); //길이초과하면 되돌리기
+        
+	}
+});
+
+// 핸드폰
+$('#ad_phone2').on('input', function(){
+	let ad_phone = $('#ad_phone2').val();
+	if(ad_phone.length == 0 || ad_phone.length == 11){
+		// 핸드폰 앞자리 010으로 통합됨
+		if(ad_phone.substring(0,3) == '010'){
+			$('#phoneChk').css('display', 'none');
+			$('#ad_phone2').css('border', '1px solid #d9d9d9');
+			$('#ad_phone2').css('outline', 'none');
+			$('#ad_phone2:hover').css('border-color', '#729ea1');
+			phone_input = 1;
+		}
+	}
+	else{
+		$('#phoneChk').css('display', 'flex');
+		$('#ad_phone2').css('border', '1px solid rgba(255, 0, 0)');
+		$('#ad_phone2').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+		phone_input = 0;
+	}
+});
+
+// 아이디
+$('#ad_id').on('input', function(){
+	let ad_id = $('#ad_id').val();
+		if(reg_id.test(ad_id) || ad_id.length == 0){
+			$('#idStr').css('display', 'none');
+			$('#ad_id').css('border', '1px solid #d9d9d9');
+			$('#ad_id').css('outline', 'none');
+			$('#idValCheck').val(1);
 		}
 		else{
-			$('#phoneChk').css('display', 'flex');
-			$('#ad_phone2').css('border', '1px solid rgba(255, 0, 0)');
-			$('#ad_phone2').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+			$('#idStr').css('display', 'flex');
+			$('#ad_id').css('border', '1px solid rgba(255, 0, 0)');
+			$('#ad_id').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+			$('#idValCheck').val(0);
 		}
-	});
-	
-	// 아이디
-	$('#ad_id').on('input', function(){
-		let ad_id = $('#ad_id').val();
-			if(reg_id.test(ad_id) || ad_id.length == 0){
-				$('#idStr').css('display', 'none');
-				$('#ad_id').css('border', '1px solid #d9d9d9');
-				$('#ad_id').css('outline', 'none');
-				$('#idValCheck').val(1);
-			}
-			else{
-				$('#idStr').css('display', 'flex');
-				$('#ad_id').css('border', '1px solid rgba(255, 0, 0)');
-				$('#ad_id').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-				$('#idValCheck').val(0);
-			}
-	});
-	
-	// 비밀번호
-	$('#ad_pwd').on('input', function(){
-		let ad_pwd = $('#ad_pwd').val();
-		if(reg_pwd.test(ad_pwd) || ad_pwd.length == 0){
-			$('#pwdStr1').css('display', 'none');
-			$('#ad_pwd').css('border', '1px solid #d9d9d9');
-			$('#ad_pwd').css('outline', 'none');
-			$('#pwdValCheck').val(1);
+});
+
+// 비밀번호
+$('#ad_pwd').on('input', function(){
+	let ad_pwd = $('#ad_pwd').val();
+	if(reg_pwd.test(ad_pwd) || ad_pwd.length == 0){
+		$('#pwdStr1').css('display', 'none');
+		$('#ad_pwd').css('border', '1px solid #d9d9d9');
+		$('#ad_pwd').css('outline', 'none');
+		$('#pwdValCheck').val(1);
+	}
+	else{
+		$('#pwdStr1').css('display', 'flex');
+		$('#ad_pwd').css('border', '1px solid rgba(255, 0, 0)');
+		$('#ad_pwd').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+		$('#pwdValCheck').val(0);
+	}
+});
+
+// 비밀번호 확인
+$('input[type=password]').on('input', function(){
+		if($('#pwdChk').val() == $('#ad_pwd').val()){
+			$('#pwdStr2').css('display', 'none');
+			$('#pwdChk').css('border', '1px solid #d9d9d9');
+			$('#pwdChk').css('outline', 'none');
 		}
 		else{
-			$('#pwdStr1').css('display', 'flex');
-			$('#ad_pwd').css('border', '1px solid rgba(255, 0, 0)');
-			$('#ad_pwd').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-			$('#pwdValCheck').val(0);
+			$('#pwdStr2').css('display', 'flex');
+			$('#pwdChk').css('border', '1px solid rgba(255, 0, 0)');
+			$('#pwdChk').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
 		}
-	});
+});
+
+// 이메일
+$('#ad_email1').on('input', function(){
+	let ad_email1 = $('#ad_email1').val();
+		if(reg_email1.test(ad_email1) || ad_email1.length == 0){
+			$('#email1Str').css('display', 'none');
+			$('#ad_email1').css('border', '1px solid #d9d9d9');
+			$('#ad_email1').css('outline', 'none');
+			$('#emailVal1Check').val(1);
+		}
+		else{
+			$('#email1Str').css('display', 'flex');
+			$('#ad_email1').css('border', '1px solid rgba(255, 0, 0)');
+			$('#ad_email1').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+			$('#emailVal1Check').val(0);
+		}
+});
+
+// 이메일2
+$('#ad_email2').on('input', function(){
+	// 값 직접 입력시에만 실행
+	$('#emailVal2Check').val(0);
+	$('#ad_email3 option').prop('selected',false);  // 이메일 select option들은 직접입력하면 초기화
+	let ad_email2 = $('#ad_email2').val();
 	
-	// 비밀번호 확인
-	$('input[type=password]').on('input', function(){
-			if($('#pwdChk').val() == $('#ad_pwd').val()){
-				$('#pwdStr2').css('display', 'none');
-				$('#pwdChk').css('border', '1px solid #d9d9d9');
-				$('#pwdChk').css('outline', 'none');
-			}
-			else{
-				$('#pwdStr2').css('display', 'flex');
-				$('#pwdChk').css('border', '1px solid rgba(255, 0, 0)');
-				$('#pwdChk').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-			}
-	});
-	
-	// 이메일
-	$('#ad_email1').on('input', function(){
-		let ad_email1 = $('#ad_email1').val();
-			if(reg_email1.test(ad_email1) || ad_email1.length == 0){
-				$('#email1Str').css('display', 'none');
-				$('#ad_email1').css('border', '1px solid #d9d9d9');
-				$('#ad_email1').css('outline', 'none');
-				$('#emailVal1Check').val(1);
-			}
-			else{
-				$('#email1Str').css('display', 'flex');
-				$('#ad_email1').css('border', '1px solid rgba(255, 0, 0)');
-				$('#ad_email1').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-				$('#emailVal1Check').val(0);
-			}
-	});
-	
-	// 이메일2
-	$('#ad_email2').on('input', function(){
-		// 값 직접 입력시에만 실행
+	if(reg_email2.test(ad_email2) || ad_email2.length == 0){
+		$('#email2Str').css('display', 'none');
+		$('#ad_email2').css('border', '1px solid #d9d9d9');
+		$('#ad_email2').css('outline', 'none');
+		$('#emailVal2Check').val(1);
+	}
+	else{
+		$('#email2Str').css('display', 'flex');
+		$('#ad_email2').css('border', '1px solid rgba(255, 0, 0)');
+		$('#ad_email2').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
 		$('#emailVal2Check').val(0);
-		$('#ad_email3 option').prop('selected',false);  // 이메일 select option들은 직접입력하면 초기화
-		let ad_email2 = $('#ad_email2').val();
-		
-		if(reg_email2.test(ad_email2) || ad_email2.length == 0){
-			$('#email2Str').css('display', 'none');
-			$('#ad_email2').css('border', '1px solid #d9d9d9');
-			$('#ad_email2').css('outline', 'none');
-			$('#emailVal2Check').val(1);
-		}
-		else{
-			$('#email2Str').css('display', 'flex');
-			$('#ad_email2').css('border', '1px solid rgba(255, 0, 0)');
-			$('#ad_email2').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-			$('#emailVal2Check').val(0);
-		}
-	});
-	
-	// 생년월일
-	$('#ad_birth').on('input', function(){
-		// 값 직접 입력시에만 실행
-		let ad_birthY = parseInt($('#ad_birth').val().substring(0,2));
-		let ad_birthM = parseInt($('#ad_birth').val().substring(2,4));
-		let ad_birthD = parseInt($('#ad_birth').val().substring(4,6));
-		let ad_birth = $('#ad_birth').val();
-		
-		//현재 년월일 받아오기
-		let date = new Date();
-		currY = date.getFullYear() % 100; // 100으로 나눈 나머지 반환 = 10의 자리년도 형식 
-		currM = date.getMonth()+1;
-		currD = date.getDate();
-		
-		// 년도로 나이 계산
-		let yearChk = (currY >= ad_birthY)? currY-ad_birthY : (100-ad_birthY)+currY;
-		
-		// let inputChk = 0; 위에 정의 됨 다른 곳에서도 써야해서
-		
-		// 값 없어지면 경고창 지우기
-		if(ad_birth.length == 0){
-			$('#birthChk').css('display', 'none');
-            $('#ad_birth').css('border', '1px solid #d9d9d9');
-            $('#ad_birth').css('outline', 'none');
-            $('#brithValCheck').val(0);
-		}
-		
-		else{
-			// 입력이 6자리면
-			if(ad_birth.length == 6){
-			inputChk=0;
-				// 1~12 월만 참 
-				if( ad_birthM >=1 && ad_birthM <=12){
-					// 2월 체크
-					if(ad_birthM == 2){
-						// 윤년일 때 29일까지
-						if(ad_birthY  % 4 == 0){
-							if(ad_birthD >= 1 && ad_birthD <= 29){
-								inputChk = 1;
-							}
-						}
-						// 윤년 아닐 때
-						else if(ad_birthD >= 1 && ad_birthD <= 28){
-							inputChk = 1;
-						}
-					}
-					//2월이 아닌 나머지 중 홀수월 && 1~30일
-					else if(ad_birthM % 2 != 0){
-						if(ad_birthD >= 1 && ad_birthD <=30){
-							inputChk = 1;
-						}
-					}
-					// 짝수월 확정, 1~31일
-					else if (ad_birthD >= 1 && ad_birthD <=31){
-						inputChk = 1;
-					}
-				} // 1~12월
-			} // 입력 6자리
-			else{ // 입력 6자리 아님
-					$('#birthChk').css('display', 'flex');
-	                $('#ad_birth').css('border', '1px solid rgba(255, 0, 0)');
-	                $('#ad_birth').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-	                $('#brithValCheck').val(0);
-				}
-		} // 0일 때 else
-		
-		// 입력 값 검사 후 정상 입력 범위일 때만 실행 
-		if( inputChk == 1){
-			// 생일 안지났으면 한살 뺴기
-            if(currM < ad_birthM || currM == ad_birthM && currD < ad_birthD){
-               yearChk = yearChk-1;
+	}
+});
 
-             }      
-            if(yearChk >= 14 && yearChk <= 60){ // 14세 이상, 60세 미만
-                
-               $('#birthChk').css('display', 'none');
-                   $('#ad_birth').css('border', '1px solid #d9d9d9');
-                   $('#ad_birth').css('outline', 'none');
-                   $('#brithValCheck').val(1);
-               
-            }
-            else{ // 14세 미만
-               $('#birthChk').css('display', 'flex');
-                   $('#ad_birth').css('border', '1px solid rgba(255, 0, 0)');
-                   $('#ad_birth').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
-                   $('#brithValCheck').val(0);
-            }
-         }
-		
-	});
+// 생년월일
+$('#ad_birth').on('input', function(){
+	// 값 직접 입력시에만 실행
+	let ad_birthY = parseInt($('#ad_birth').val().substring(0,2));
+	let ad_birthM = parseInt($('#ad_birth').val().substring(2,4));
+	let ad_birthD = parseInt($('#ad_birth').val().substring(4,6));
+	let ad_birth = $('#ad_birth').val();
 	
-	// input type이 number일 때, maxlength 속성이 존재하는 요소 선택
-	$('input[type=number][maxlength]').on('input', function(event) { 
-	    var num = $(this);                             // this = 이벤트 발생시킨 요소
-	    var maxlength = num.attr('maxlength');         // maxlength 속성 값을 불러옴
-	    var numval = num.val();
-	    if (numval.length >= maxlength) {
-	        num.val(numval.substr(0, maxlength)); //길이초과하면 되돌리기
-	        
-	        // 초과 입력하면 경고창 안지워져서 수동 삭제
-	        if(num.attr('class') == 'ad_phone2'){
-	        	$('#phoneChk').css('display', 'none');
-	        	$('#ad_phone2').css('border', '1px solid #d9d9d9');
-                $('#ad_phone2').css('outline', 'none');
-	        }
-	        else  if(num.attr('class') == 'ad_birth' && inputChk == 1){
-	        	$('#birthChk').css('display', 'none');
-	        	$('#ad_birth').css('border', '1px solid #d9d9d9');
-                $('#ad_birth').css('outline', 'none');
-	        }
-    	}
-	});
+	//현재 년월일 받아오기
+	let date = new Date();
+	currY = date.getFullYear() % 100; // 100으로 나눈 나머지 반환 = 10의 자리년도 형식 
+	currM = date.getMonth()+1;
+	currD = date.getDate();
+	
+	// 년도로 나이 계산 
+	let yearChk = (currY >= ad_birthY)? currY-ad_birthY : (100-ad_birthY)+currY;
+	
+	// 생년월일 체크용
+	let b_inputChk = 0;
+	
+	// 값 없어지면 경고창 지우기
+	if(ad_birth.length == 0){
+		$('#birthChk').css('display', 'none');
+        $('#ad_birth').css('border', '1px solid #d9d9d9');
+        $('#ad_birth').css('outline', 'none');
+        $('#brithValCheck').val(0);
+	}
+	
+	else{
+		// 입력이 6자리면
+		if(ad_birth.length == 6){
+		
+		// 주민번호 뒷자리 판단
+		jender_input = (currY >= ad_birthY)? 2 : 1; // 2이면 뒷자리 3,4 1이면 1,2
+		
+			// 1~12 월만 참 
+			if( ad_birthM >=1 && ad_birthM <=12){
+				// 2월 체크
+				if(ad_birthM == 2){
+					// 윤년일 때 29일까지
+					if(ad_birthY  % 4 == 0){
+						if(ad_birthD >= 1 && ad_birthD <= 29){
+							b_inputChk = 1;
+						}
+					}
+					// 윤년 아닐 때
+					else if(ad_birthD >= 1 && ad_birthD <= 28){
+						b_inputChk = 1;
+					}
+				}
+				//2월이 아닌 나머지 중 홀수월 && 1~30일
+				else if(ad_birthM % 2 != 0){
+					if(ad_birthD >= 1 && ad_birthD <=30){
+						b_inputChk = 1;
+					}
+				}
+				// 짝수월 확정, 1~31일
+				else if (ad_birthD >= 1 && ad_birthD <=31){
+					b_inputChk = 1;
+				}
+			} // 1~12월
+		} // 입력 6자리
+		else{ // 입력 6자리 아님
+				$('#birthChk').css('display', 'flex');
+                $('#ad_birth').css('border', '1px solid rgba(255, 0, 0)');
+                $('#ad_birth').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+                $('#brithValCheck').val(0);
+		}
+	} // 0일 때 else
+	
+	// 입력 값 검사 후 정상 입력 범위일 때만 실행 
+	if(b_inputChk == 1){
+		// 생일 안지났으면 한살 뺴기
+        if(currM < ad_birthM || currM == ad_birthM && currD < ad_birthD){
+           yearChk = yearChk-1;
+
+         }      
+        if(yearChk >= 14 && yearChk <= 60){ // 14세 이상, 60세 미만
+            
+           $('#birthChk').css('display', 'none');
+           $('#ad_birth').css('border', '1px solid #d9d9d9');
+           $('#ad_birth').css('outline', 'none');
+           $('#brithValCheck').val(1);
+           
+           // 생년월일 변경시 성별 검사
+           jenderChk();
+           
+        }
+        else{ // 14세 미만
+           $('#birthChk').css('display', 'flex');
+           $('#ad_birth').css('border', '1px solid rgba(255, 0, 0)');
+           $('#ad_birth').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+           $('#brithValCheck').val(0);
+        }
+     }
 	
 });
+	
+
+
+// 성별 검사
+function jenderChk(){
+	let ad_jender = $('#ad_jender').val();
+	let jenderChk =0;
+	if(jender_input == 1){
+		if(ad_jender == 1 || ad_jender == 2){
+			jenderChk = 1;
+		}
+	}
+	else if(jender_input == 2){
+		if(ad_jender == 3 || ad_jender == 4){
+			jenderChk = 1;
+		}
+	}
+	else{
+		jenderChk = 0;
+	}
+	
+	if(jenderChk == 1){
+		$('#jenderChk').css('display', 'none');
+		$('#ad_jender').css('border', '1px solid #d9d9d9');
+		$('#ad_jender').css('outline', 'none');
+	}
+	else{
+		$('#jenderChk').css('display', 'flex');
+			$('#ad_jender').css('border', '1px solid rgba(255, 0, 0)');
+			$('#ad_jender').css('outline', '3px solid rgba(255, 0, 0, 0.3)');
+	}
+}
 
  
 // 이메일 값주기
@@ -486,3 +542,47 @@ btn.on("click", () => {
         }
     }).open();
 });
+
+// 유니크 컬럼들 ajax로 조회하기 
+// 매개변수 = (컨텍스트경로, 변경될 dev id, 입력한 input id)
+function uniqueChk(path, elementId, inputId){
+	
+	// 매개변수로 요소 호출하기, 각자의 변수로 함수 재사용
+	let input = $(inputId)
+	let element = $(elementId);
+	
+	// 객체의 key, value로 넘겨 value에 따라 다른 조건으로 실행
+	let elementJson = {};
+	
+	//객체에 value와 id를 저장
+	elementJson.value = input.val();
+	
+	// 이메일은 값을 둘다 받아야한다.
+	if(elementId == '#emailUnique'){
+		elementJson.id = 'ad_email';
+		elementJson.value = $('#ad_email1').val()+"@"+$('#ad_email2').val(); 
+	}
+	else if(elementId == '#phoneUnique'){
+		elementJson.id = 'ad_phone';
+	}
+	else if(elementId == '#empnumlUnique'){
+		elementJson.id = 'empnum';
+		
+	}
+	
+	// ajax에서 전달 된 객체의 key와 value에 getParameter로 접근
+	// 성공하면 actionResult에서 요청된 요소 별로 다르게 input 요소를 hidden으로 추가하여 
+	// submit 할 때 값을 검사한다. !(요소)는 존재여부로 검사가 안되어 val()==1 로 submit 조건 수정
+	$.ajax({
+		url: path+"/uniqueCheck.ad",
+		data:elementJson, // get 방식으로 넘기기에 parameter로 받기	
+		type: 'POST',
+		success: function(result){
+			element.html(result);
+		},
+		error: function(){
+			alert("error");
+		}
+	});
+	
+}
