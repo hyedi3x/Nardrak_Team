dda25be8c5af798a0ee627069c8480384e60efd0
wpzmfhtm <wpzmfhtm@naver.com>
2025-02-12 02:15:26 +0900

    [feat] ImageUpload.js
    - formId 형식( form-${image.image_id} )
    - actionType이 update면 수정 처리 delete면 삭제 처리로 action 속성 설정
    - 삭제를 하기 전 삭제 여부를 미리 confirm으로 확인
    - form.submit()를 써서 action 속성에 설정한 컨트롤러로 타고 들어가서 처리

---
+++ nardrak_spring_mvc/src/main/webapp/resources/js/admin/image/ImageUpload.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+function submitForm(formId, actionType) {
+    const form = document.getElementById(formId);
+
+    // actionType에 따라 form의 action을 다르게 설정
+    if (actionType === 'update') {
+        form.action = "updateImage.img";  // 수정
+        form.submit();
+    } else if (actionType === 'delete') {
+        // 삭제 확인
+        if (confirm("정말 삭제하시겠습니까?")) {
+            form.action = "deleteImage.img";  // 삭제
+            form.submit();
+        }
+    }
+}
