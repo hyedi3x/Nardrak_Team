57537b9d11c2ab8d4281be08ab1269bfa02fe326
hyedi3x <hyedi3x@gmail.com>
2025-02-17 03:21:54 +0900

    [fix] InquiryDTO.java (주석 수정 및 i_num, adI_num 제거)
    - i_num과 adI_num은 각 테이블의 pk 값으로 화면에 표출하지 않을 예정

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dto/InquiryDTO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dto/InquiryDTO.java
@@ -4,8 +4,7 @@ import java.sql.Timestamp;
 
 public class InquiryDTO {
 	
-	// Fields for customer inquiry (inquiryCS_tb)
-    private String i_num;           // 문의 ID(PK) /1:1내역에서는 (FK)
+	// 멤버 변수 : customer inquiry (inquiryCS_tb)
     private String i_title;         // 문의 제목
     private String i_category;      // 문의 유형
     private String i_content;       // 문의 내용
@@ -13,8 +12,7 @@ public class InquiryDTO {
     private Timestamp i_writeDate;  // 작성일
     private String cs_id;           // 고객 ID(FK)
 
-    // Fields for admin response (inquiryAD_tb)
-    private String adI_num;         // 답변 ID(PK)
+    // 멤버 변수 : admin response (inquiryAD_tb)
     private String adI_reply;       // 관리자 답변 내용
     private Timestamp adI_replyDate;// 답변 일시
     private String adI_status;      // 문의 상태 (pending, answered)
@@ -27,18 +25,16 @@ public class InquiryDTO {
 	}
 
 	//매개변수 생성자
-	public InquiryDTO(String i_num, String i_title, String i_category, String i_content, String i_imgUpload,
-			Timestamp i_writeDate, String cs_id, String adI_num, String adI_reply, Timestamp adI_replyDate,
+	public InquiryDTO(String i_title, String i_category, String i_content, String i_imgUpload,
+			Timestamp i_writeDate, String cs_id, String adI_reply, Timestamp adI_replyDate,
 			String adI_status, String ad_id, String ad_dep) {
 		super();
-		this.i_num = i_num;
 		this.i_title = i_title;
 		this.i_category = i_category;
 		this.i_content = i_content;
 		this.i_imgUpload = i_imgUpload;
 		this.i_writeDate = i_writeDate;
 		this.cs_id = cs_id;
-		this.adI_num = adI_num;
 		this.adI_reply = adI_reply;
 		this.adI_replyDate = adI_replyDate;
 		this.adI_status = adI_status;
@@ -47,14 +43,6 @@ public class InquiryDTO {
 	}
 
 	//getter setter
-	public String getI_num() {
-		return i_num;
-	}
-
-	public void setI_num(String i_num) {
-		this.i_num = i_num;
-	}
-
 	public String getI_title() {
 		return i_title;
 	}
@@ -103,14 +91,6 @@ public class InquiryDTO {
 		this.cs_id = cs_id;
 	}
 
-	public String getAdI_num() {
-		return adI_num;
-	}
-
-	public void setAdI_num(String adI_num) {
-		this.adI_num = adI_num;
-	}
-
 	public String getAdI_reply() {
 		return adI_reply;
 	}
@@ -154,9 +134,9 @@ public class InquiryDTO {
 	//toString
 	@Override
 	public String toString() {
-		return "InquiryDTO [i_num=" + i_num + ", i_title=" + i_title + ", i_category=" + i_category + ", i_content="
+		return "InquiryDTO [i_title=" + i_title + ", i_category=" + i_category + ", i_content="
 				+ i_content + ", i_imgUpload=" + i_imgUpload + ", i_writeDate=" + i_writeDate + ", cs_id=" + cs_id
-				+ ", adI_num=" + adI_num + ", adI_reply=" + adI_reply + ", adI_replyDate=" + adI_replyDate
+				+ ", adI_reply=" + adI_reply + ", adI_replyDate=" + adI_replyDate
 				+ ", adI_status=" + adI_status + ", ad_id=" + ad_id + ", ad_dep=" + ad_dep + "]";
 	}
 }
