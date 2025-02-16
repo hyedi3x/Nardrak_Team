cdd0fbd8bc744e09ca4c6b7ae9c2ef9f866adb1d
wpzmfhtm <wpzmfhtm@naver.com>
2025-02-16 18:25:39 +0900

    [fix] FavoriteService.java(ad_id 세션으로 대체)
    - 파라미터 null 체크 후 처리
    - 아직 손 볼 곳이 좀 있지만 일단 INSERT/DELETE는 작동됨

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/FavoriteService.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/service/FavoriteService.java
@@ -1,13 +1,15 @@
 package com.spring.nardrak_mvc.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.ui.Model;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.spring.nardrak_mvc.dto.FavoriteDTO;
 
 public interface FavoriteService {
 
@@ -16,5 +18,8 @@ public interface FavoriteService {
 	public void favoriteAction(HttpServletRequest request,HttpServletResponse response,Model model)  
 		    throws ServletException, IOException;
 	
+	// ======================= [선호 여행지 목록 조회] =======================
+	public List<FavoriteDTO> favoriteList(HttpServletRequest request,HttpServletResponse response,Model model)  
+			throws ServletException, IOException;
 
 }
