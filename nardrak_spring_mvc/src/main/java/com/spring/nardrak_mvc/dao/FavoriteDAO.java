cafa69aa42e23c6c694a809ad8dbbcd2dcd0ca9c
wpzmfhtm <wpzmfhtm@naver.com>
2025-02-16 18:20:55 +0900

    [fix] FavoriteDAO.java(DAO 패키지 경로 변경)

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAO.java
@@ -1,6 +1,7 @@
 package com.spring.nardrak_mvc.dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 import com.spring.nardrak_mvc.dto.FavoriteDTO;
 
@@ -11,4 +12,7 @@ public interface FavoriteDAO {
 	
 	// ======================= [선호 여행지 제거] =======================
 	public void removeFavorite(HashMap<String,String>map);
+	
+	// ======================= [선호 여행지 목록 조회] =======================
+	public List<FavoriteDTO> getFavoriteList(String ad_id);
 }
--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAOImpl.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAOImpl.java
@@ -1,6 +1,7 @@
 package com.spring.nardrak_mvc.dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +15,25 @@ public class FavoriteDAOImpl implements FavoriteDAO {
     @Autowired
     private SqlSessionTemplate sqlSession;
 
-    String favorite = "com.spring.nardrak_mvc.dao.CustomerDAO.";
+    String favorite = "com.spring.nardrak_mvc.dao.FavoriteDAO.";
 
     // ======================= [선호 여행지 추가] =======================
     @Override
     public void addFavorite(FavoriteDTO dto) {
         // MyBatis를 사용하여 선호 여행지 추가
-        sqlSession.insert(favorite + ".addFavorite", dto);
+        sqlSession.insert(favorite + "addFavorite", dto);
     }
 
     // ======================= [선호 여행지 제거] =======================
     @Override
     public void removeFavorite(HashMap<String, String> map) {
         // MyBatis를 사용하여 선호 여행지 제거
-        sqlSession.delete(favorite + ".removeFavorite", map);
+        sqlSession.delete(favorite + "removeFavorite", map);
     }
+
+    // ======================= [선호 여행지 목록 조회] =======================
+	@Override
+	public List<FavoriteDTO> getFavoriteList(String ad_id) {
+		return sqlSession.selectList(favorite + "getFavoriteList", ad_id);
+	}
 }
