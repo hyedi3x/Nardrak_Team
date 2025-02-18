3eb148caf768252d74974633dcd6fa25a4769dfd
wpzmfhtm <wpzmfhtm@naver.com>
2025-02-18 17:55:40 +0900

    [fix] LocalDAO.java(국내 여행지 상세정보 조회(1건)  기능 추가)

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/LocalDAO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/LocalDAO.java
@@ -7,13 +7,15 @@ import com.spring.nardrak_mvc.dto.LocalDTO;
 
 public interface LocalDAO {
 
-	// 국내 여행지 정보 등록
+	// ======================= [국내 여행지 정보 등록] =======================
 	public int InsertTour(LocalDTO dto);
 	
-	// 여행지 정보 전부 가져오기
+	// ======================= [국내 여행지 정보 전체 조회] =======================
 	public List<LocalDTO> localList(Map<String, Object> map);
 	
-	// 총 지역정보 개수
+	// ======================= [국내 여행지 정보 전체 개수] =======================
 	public int localCnt();
 
+	// ======================= [국내 여행지 상세정보 조회(1건)] =======================
+	public LocalDTO localInfo(String local_title);
 }
