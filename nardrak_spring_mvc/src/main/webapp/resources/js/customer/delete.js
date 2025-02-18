650b7c4a7898b94ef82ce54b0dc62149bb33009a
meldy22 <anzngksduswn@naver.com>
2025-02-18 12:44:23 +0900

    [fix] deleteInfo.jsp & delete.js(취소 수정)
    - 취소 누르면 alert 띄우기 추가

--- nardrak_spring_mvc/src/main/webapp/resources/js/customer/delete.js
+++ nardrak_spring_mvc/src/main/webapp/resources/js/customer/delete.js
@@ -1,32 +1,38 @@
 /**
  * 일반회원탈퇴 js
  */
+	
+function myInfoBTN(path){
+	if(confirm('마이페이지로 돌아가시겠습니까?')){
+		window.location=path+"/myInfo.do";
+	}
+}
  
-    function submitForm() {
-        // 약관 동의 여부 체크
-        var agreeChecked = document.getElementById("agree1").checked;
+function submitForm() {
+    // 약관 동의 여부 체크
+    var agreeChecked = document.getElementById("agree1").checked;
 
-        // 탈퇴 사유 선택 여부 체크
-        var reasonChecked = false;
-        var reasonRadios = document.getElementsByName("levCd");
-        for (var i = 0; i < reasonRadios.length; i++) {
-            if (reasonRadios[i].checked) {
-                reasonChecked = true;
-                break;
-            }
+    // 탈퇴 사유 선택 여부 체크
+    var reasonChecked = false;
+    var reasonRadios = document.getElementsByName("levCd");
+    for (var i = 0; i < reasonRadios.length; i++) {
+        if (reasonRadios[i].checked) {
+            reasonChecked = true;
+            break;
         }
-
-        // 약관에 동의하지 않았거나, 탈퇴 사유를 선택하지 않은 경우
-        if (!agreeChecked) {
-            alert("탈퇴 약관에 동의해주세요.");
-            return false; // 폼 제출을 막음
-        }
-
-        if (!reasonChecked) {
-            alert("탈퇴 사유를 선택해주세요.");
-            return false; // 폼 제출을 막음
-        }
-
-        // 모든 조건을 만족하면 폼을 제출
-        return true;
     }
+
+    // 약관에 동의하지 않았거나, 탈퇴 사유를 선택하지 않은 경우
+    if (!agreeChecked) {
+        alert("탈퇴 약관에 동의해주세요.");
+        return false; // 폼 제출을 막음
+    }
+
+    if (!reasonChecked) {
+        alert("탈퇴 사유를 선택해주세요.");
+        return false; // 폼 제출을 막음
+    }
+
+    // 모든 조건을 만족하면 폼을 제출
+    return true;
+}
