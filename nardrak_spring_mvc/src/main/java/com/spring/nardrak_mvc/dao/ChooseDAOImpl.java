1b19e78d4ce0697019439c06adcb2b82e0c0b36f
kilminjong <qkqk0110@naver.com>
2025-02-19 21:25:21 +0900

    [feat] ChooseDAOImpl.java
    - dao 인터페이스 생성및 daoImpl 생성
    - 선호 여행지 insert, select 기능 추가

---
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/ChooseDAOImpl.java
@@ -0,0 +1,44 @@
+package com.spring.nardrak_mvc.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.nardrak_mvc.dto.ChooseDTO;
+
+@Repository
+public class ChooseDAOImpl implements ChooseDAO {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	String choose = "com.spring.nardrak_mvc.dao.ChooseDAO.";
+
+	// ================= [추천 여행지 정보 등록] ======================
+	@Override
+	public int InsertChoose(ChooseDTO dto) {
+	
+		return sqlSession.insert(choose + "InsertChoose", dto);
+	}
+	
+	// ================= [국내 여행지 정보 전체 조회] ===================
+	@Override
+	public List<ChooseDTO> ChooseList() {
+		
+		return sqlSession.selectList(choose + "ChooseList");
+	}
+
+	// ================= [추천 여행지 상세정보 조회(1건)] ================
+	@Override
+	public ChooseDTO chooseInfo(String ch_title1) {
+		System.out.println("LocalDAO - chooseInfo()");
+
+		return sqlSession.selectOne(choose + "chooseInfo", ch_title1);
+	}
+	
+
+	
+}
