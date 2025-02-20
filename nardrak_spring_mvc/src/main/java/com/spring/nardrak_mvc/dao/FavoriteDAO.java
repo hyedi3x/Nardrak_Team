1ec31c1037bbd08a3f6d963135924e18ed19acc3
wpzmfhtm <wpzmfhtm@naver.com>
2025-02-20 14:38:28 +0900

    [fix] FavoriteDAO.java
    - 선호 여행지 선호 여부 추가
    - 콘솔에 메서드 이름 나오게 코드 추가

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAO.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/FavoriteDAO.java
@@ -2,11 +2,15 @@ package com.spring.nardrak_mvc.dao;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.spring.nardrak_mvc.dto.FavoriteDTO;
 
 public interface FavoriteDAO {
-
+	
+	// ======================= [선호 여행지 추가] =======================
+	public int favoriteSelectCnt(Map<String, Object> map);
+	
 	// ======================= [선호 여행지 추가] =======================
 	public void addFavorite(FavoriteDTO dto);
 	
@@ -14,5 +18,5 @@ public interface FavoriteDAO {
 	public void removeFavorite(HashMap<String,String>map);
 	
 	// ======================= [선호 여행지 목록 조회] =======================
-	public List<FavoriteDTO> getFavoriteList(String ad_id);
+	public List<FavoriteDTO> getFavoriteList(String cs_id);
 }
