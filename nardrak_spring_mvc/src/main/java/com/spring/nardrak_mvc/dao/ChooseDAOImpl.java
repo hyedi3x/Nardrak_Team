0675c6e9aa776d61e6a7adec690ac562ed3faa1a
kilminjong <qkqk0110@naver.com>
2025-02-20 23:05:28 +0900

    [feat] ChooseDAOImpl.java
    - 상세정보 수정 구현

--- nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/ChooseDAOImpl.java
+++ nardrak_spring_mvc/src/main/java/com/spring/nardrak_mvc/dao/ChooseDAOImpl.java
@@ -20,13 +20,15 @@ public class ChooseDAOImpl implements ChooseDAO {
 	// ================= [추천 여행지 정보 등록] ======================
 	@Override
 	public int InsertChoose(ChooseDTO dto) {
-	
+		System.out.println("ChooseDAO - InsertChoose()");
+		
 		return sqlSession.insert(choose + "InsertChoose", dto);
 	}
 	
 	// ================= [국내 여행지 정보 전체 조회] ===================
 	@Override
 	public List<ChooseDTO> ChooseList() {
+		System.out.println("ChooseDAO - ChooseList()");
 		
 		return sqlSession.selectList(choose + "ChooseList");
 	}
@@ -34,9 +36,17 @@ public class ChooseDAOImpl implements ChooseDAO {
 	// ================= [추천 여행지 상세정보 조회(1건)] ================
 	@Override
 	public ChooseDTO chooseInfo(String ch_title1) {
-		System.out.println("LocalDAO - chooseInfo()");
+		System.out.println("ChooseDAO - chooseInfo()");
 
 		return sqlSession.selectOne(choose + "chooseInfo", ch_title1);
+	}
+	
+	// ================= [국내 여행지 상세정보 수정] ====================
+	@Override
+	public int updateChoose(ChooseDTO dto) {
+		System.out.println("ChooseDAO - updateChoose()");
+		
+		return sqlSession.update(choose + "updateChoose",dto);
 	}
 	
 
